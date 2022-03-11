9331bd16600bbaa38e01f4bac57185d694e73dbe
mjt-21 <mjtcraft@gmail.com>
2022-03-11 14:24:20 -0800

    fixed issues, further completion on table mapping page

--- gui.py
+++ gui.py
@@ -1,3 +1,4 @@
+import time
 from closest_neighbor import Ui_MainWindow
 import main
 
@@ -7,148 +8,181 @@ from PyQt5 import QtGui as qtg
 
 class MainWindow(qtw.QMainWindow):
 
-    def __init__(self, *args, **kwargs):
+	def __init__(self, *args, **kwargs):
 
-        ################################################
-        #                                              #
-        #             WINDOW INITIALIZATION            #
-        #                                              #
-        ################################################
+		################################################
+		#                                              #
+		#             WINDOW INITIALIZATION            #
+		#                                              #
+		################################################
 
-        super().__init__(*args, **kwargs)
+		super().__init__(*args, **kwargs)
 
-        ##### ----- MAIN WINDOW ----- #####
-        # Initialize main window
-        self.ui = Ui_MainWindow()
-        self.ui.setupUi(self)
-        self.setWindowTitle("Closest Neighbor")
-        self.setFixedSize(800, 600) 
+		##### ----- MAIN WINDOW ----- #####
+		# Initialize main window
+		self.ui = Ui_MainWindow()
+		self.ui.setupUi(self)
+		self.setWindowTitle("Closest Neighbor")
+		self.setFixedSize(800, 600) 
 
-        # Start application on connection options page
-        self.connectionOptionsPage()
+		# Start application on connection options page
+		self.connectionOptionsPage()
 
-        self.conn_error_box = qtw.QMessageBox()
-        self.conn_error_box.setIcon(qtw.QMessageBox.Critical)
-        self.conn_error_box.setText("ERROR:\n\nA connection could not be not established.")
-        self.conn_error_box.setWindowTitle("Connection Error")
+		self.conn_error_box = qtw.QMessageBox()
+		self.conn_error_box.setIcon(qtw.QMessageBox.Critical)
+		self.conn_error_box.setText("ERROR:\n\nA connection could not be not established.")
+		self.conn_error_box.setWindowTitle("Connection Error")
 
-        ################################################
-        #                                              #
-        #                    EVENTS                    #
-        #                                              #
-        ################################################
+		################################################
+		#                                              #
+		#                    EVENTS                    #
+		#                                              #
+		################################################
 
-        ##### ----- TOP NAV ----- #####
+		##### ----- TOP NAV ----- #####
 
-        # Link github_action button to project's GitHub page
-        self.ui.github_action.triggered.connect(self.openUrl)
+		# Link github_action button to project's GitHub page
+		self.ui.github_action.triggered.connect(self.openUrl)
 
-        ##### ----- DATA CONNECTION OPTIONS PAGE ----- #####
+		##### ----- DATA CONNECTION OPTIONS PAGE ----- #####
 
-        # Redirect to SQL Connection Manager page when SQL is chosen
-        self.ui.sql_option_button.clicked.connect(self.SQLConnectionPage)
+		# Redirect to SQL Connection Manager page when SQL is chosen
+		self.ui.sql_option_button.clicked.connect(self.SQLConnectionPage)
 
-        ##### ----- SQL CONNECTION MANAGER PAGE ----- #####
+		##### ----- SQL CONNECTION MANAGER PAGE ----- #####
 
-        global driver, server, user_name, password, database
+		global connString, driver, server, user_name, password, database, table, timeout
+		connString = driver = server = user_name = password = database = table = False
+		timeout = 1
 
-        # Redirect to Data Connection Options page when cancel is clicked
-        self.ui.sql_cancel_button.clicked.connect(self.connectionOptionsPage)
-        # Redirect to Table Mapping page when ok is clicked
-        self.ui.sql_ok_button.clicked.connect(self.tablePage)
+		# Redirect to Data Connection Options page when cancel is clicked
+		self.ui.sql_cancel_button.clicked.connect(self.connectionOptionsPage)
+		# Redirect to Table Mapping page when ok is clicked
+		self.ui.sql_ok_button.clicked.connect(self.tablePage)
 
-        # Populate SQL Driver ComboBox with pyodbc drivers from main
-        self.ui.sql_driver_box.addItems(main.pyodbc_drivers)
+		# Populate SQL Driver ComboBox with pyodbc drivers from main
+		self.ui.sql_driver_box.addItems(main.pyodbc_drivers)
+		
+		self.ui.sql_auth_button.clicked.connect(self.useSQLAuth)
+		self.ui.windows_auth_button.clicked.connect(self.useWindowsAuth)
+		self.ui.set_authentication_button.clicked.connect(self.updateDatabaseList)
+		self.ui.sql_ok_button.clicked.connect(self.updateTableList)
 
-        self.ui.sql_test_connection_button.clicked.connect(self.testSQLConnection)
-        self.ui.sql_auth_button.clicked.connect(self.useSQLAuth)
-        self.ui.windows_auth_button.clicked.connect(self.useWindowsAuth)
-        self.ui.set_authentication_button.clicked.connect(self.updateDatabaseList)
-        self.ui.sql_ok_button.clicked.connect(self.updateTableList)
-        
-        ##### ----- CALCULATION PAGE ----- #####
-        # Redirect to SQL Connection Manager page when cancel is clicked
-        self.ui.table_cancel_button.clicked.connect(self.SQLConnectionPage)
+		##### ----- TABLE MAPPING PAGE ----- #####
+		# Redirect to SQL Connection Manager page when cancel is clicked
+		self.ui.table_cancel_button.clicked.connect(self.SQLConnectionPage)
+		self.ui.sql_table_box.currentIndexChanged.connect(self.updateColumnList)
 
 
-    ################################################
-    #                                              #
-    #                   FUNCTIONS                  #
-    #                                              #
-    ################################################
+	################################################
+	#                                              #
+	#                   FUNCTIONS                  #
+	#                                              #
+	################################################
 
-    ##### ----- TOP NAV ----- #####
-    def openUrl(self):
-        url = qtc.QUrl('https://github.com/MJTFreeTime/Closest-Neighbor')
-        if not qtg.QDesktopServices.openUrl(url):
-            qtg.QMessageBox.warning(self, 'Open Url', 'Could not open url')
+	##### ----- TOP NAV ----- #####
+	def openUrl(self):
+		url = qtc.QUrl('https://github.com/mjt-21/Closest-Neighbor')
+		if not qtg.QDesktopServices.openUrl(url):
+			qtg.QMessageBox.warning(self, 'Open Url', 'Could not open url')
 
-    # Function to switch over to Data Connection Options page
-    def connectionOptionsPage(self):
-        self.ui.main_pages.setCurrentWidget(self.ui.connection_page)
+	# Function to switch over to Data Connection Options page
+	def connectionOptionsPage(self):
+		self.ui.main_pages.setCurrentWidget(self.ui.connection_page)
 
-    # Function to switch over to SQL Connection Manager page
-    def SQLConnectionPage(self):
-        self.ui.main_pages.setCurrentWidget(self.ui.sql_page)
+	# Function to switch over to SQL Connection Manager page
+	def SQLConnectionPage(self):
+		self.ui.main_pages.setCurrentWidget(self.ui.sql_page)
 
-    # Function to switch over to Calculation page
-    def tablePage(self):
-        self.ui.main_pages.setCurrentWidget(self.ui.table_page)
+	# Function to switch over to Calculation page
+	def tablePage(self):
+		self.ui.main_pages.setCurrentWidget(self.ui.table_page)
 
-    def useSQLAuth(self):
-        self.ui.user_name_label.setEnabled(True)
-        self.ui.user_name_input.setEnabled(True)
-        self.ui.password_label.setEnabled(True)
-        self.ui.password_input.setEnabled(True)
+	def useSQLAuth(self):
+		self.ui.user_name_label.setEnabled(True)
+		self.ui.user_name_input.setEnabled(True)
+		self.ui.password_label.setEnabled(True)
+		self.ui.password_input.setEnabled(True)
 
-    def useWindowsAuth(self):
-        self.ui.user_name_label.setDisabled(True)
-        self.ui.user_name_input.setDisabled(True)
-        self.ui.password_label.setDisabled(True)
-        self.ui.password_input.setDisabled(True)
+	def useWindowsAuth(self):
+		self.ui.user_name_label.setDisabled(True)
+		self.ui.user_name_input.setDisabled(True)
+		self.ui.password_label.setDisabled(True)
+		self.ui.password_input.setDisabled(True)
 
-    def updateConnDetails(self):
-        global driver, server, user_name, password, database
-        driver = self.ui.sql_driver_box.currentText()
-        server = self.ui.server_name_input.text()
-        user_name = self.ui.user_name_input.text()
-        password = self.ui.password_input.text()
-        database = self.ui.database_name_input.currentText()
+	def updateConnDetails(self):
+		global connString, driver, server, user_name, password, database, table, timeout
 
-    def testSQLConnection(self):
-        self.updateConnDetails()
-        if main.testConnection(driver, server, user_name, password, database):
-            return True
-        else:
-            return False
+		driver = self.ui.sql_driver_box.currentText()
+		server = self.ui.server_name_input.text()
+		user_name = self.ui.user_name_input.text()
+		password = self.ui.password_input.text()
+		database = self.ui.database_name_input.currentText()
+		table = self.ui.sql_table_box.currentText()
 
-    def updateDatabaseList(self):
-        self.updateConnDetails()
-        if (self.testSQLConnection):
-            self.ui.database_name_label.setEnabled(True)
-            self.ui.database_name_input.setEnabled(True)
-            databases = main.getDatabases(driver, server, user_name, password, database)
-            if databases:
-                self.ui.database_name_input.clear()
-                self.ui.database_name_input.addItems(databases)
-        else:
-            self.conn_error_box.exec_()
+		# Only update connection string variable if all NECESSARY variables are defined (are not False)
+		if (driver and server and timeout):
+			if (user_name or password):
+				connString = "DRIVER={" + driver + "};" + "SERVER=" + server + ";DATABASE=" + database + ";UID=" + user_name + ";PWD=" + password + ";TRUSTED_CONNECTION=no"
+			else:
+				connString = "DRIVER={" + driver + "};" + "SERVER=" + server + ";DATABASE=" + database + ";TRUSTED_CONNECTION=yes"
 
-    def updateTableList(self):
-        self.updateConnDetails()
-        if (self.testSQLConnection):
-            tables = main.getTables(driver, server, user_name, password, database)
-            if tables:
-                self.ui.sql_table_box.clear()
-                self.ui.sql_table_box.addItems(tables)
-        else:
-            self.conn_error_box.exec_()
-        
+	def testSQLConnection(self):
+		if (main.testConnection(connString, timeout)):
+			return True
+		else:
+			self.conn_error_box.exec()
+			return False
+
+	def updateDatabaseList(self):
+		self.updateConnDetails()
+
+		databases = main.getDatabases(connString, timeout)
+		self.ui.database_name_input.clear()
+
+		if databases == 1:
+			self.conn_error_box.exec()
+			self.ui.database_name_label.setDisabled(True)
+			self.ui.database_name_input.setDisabled(True)
+		else:
+			self.ui.database_name_label.setEnabled(True)
+			self.ui.database_name_input.setEnabled(True)
+			self.ui.database_name_input.addItems(databases)
+
+	def updateTableList(self):
+		self.updateConnDetails()
+
+		tables = main.getTables(connString, database, timeout)
+
+		self.ui.sql_table_box.clear()
+
+		if tables == 1:
+			self.conn_error_box.exec()
+		else:
+			self.ui.sql_table_box.addItems(tables)
+
+	def updateColumnList(self):
+		self.updateConnDetails()
+
+		columns = main.getColumns(connString, table, timeout)
+
+		self.ui.uid_column_box.clear()
+		self.ui.latitude_column_box.clear()
+		self.ui.longitude_column_box.clear()
+
+		if columns == 1:
+			self.conn_error_box.exec()
+		else:
+			self.ui.uid_column_box.addItems(columns)
+			self.ui.latitude_column_box.addItems(columns)
+			self.ui.longitude_column_box.addItems(columns)
+			
+		
 
 if __name__ == '__main__':
-    app = qtw.QApplication([])
+	app = qtw.QApplication([])
 
-    mainWindow = MainWindow()
-    mainWindow.show()
+	mainWindow = MainWindow()
+	mainWindow.show()
 
-    app.exec()
+	app.exec()
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import math
 import pyodbc
 import time
-import threading
 import functions
 
 # Define a list of available pyodbc drivers
@@ -13,74 +12,69 @@ server = 'MSI'
 db = 'Closest_Neighbor'
 table = 'Lat_Longs'
 
-def isSQLAuth(user_name, password):
-    if (user_name.strip() or password.strip()):
-        return True
-    else:
-        return False
+def testConnection(connString, timeout):
+	try:
+		conn = pyodbc.connect(connString, timeout=timeout)
+		conn.close()
+		return True
+	except:
+		return False
 
-def testConnection(driver, server, user_name, password, database):
-    conn = None
-    if isSQLAuth(user_name, password):
-        try:
-            conn = pyodbc.connect(driver=driver, host=server, uid=user_name, pwd=password, database=database, trusted_connection='no')
-            conn.close()
-            return True;
-        except:
-            return False;
-    else:
-        try:
-            conn = pyodbc.connect(driver=driver, host=server, database=database, trusted_connection='yes')
-            conn.close()
-            return True;
-        except:
-            return False;
+def getDatabases(connString, timeout):
+	if (not testConnection(connString, timeout=timeout)):
+		return 1
 
-def getDatabases(driver, server, user_name, password, database):
-    try:
-        if isSQLAuth(user_name, password):
-                conn = pyodbc.connect(driver=driver, host=server, uid=user_name, pwd=password, database=database, trusted_connection='no')
-        else:
-            conn = pyodbc.connect(driver=driver, host=server, database=database, trusted_connection='yes')
-    except:
-        return []
+	conn = pyodbc.connect(connString, timeout=timeout)
+	cursor = conn.cursor()
+	query = '''SELECT name FROM sys.databases'''
+	cursor.execute(query)
+	data = cursor.fetchall()
+	cursor.close()
+	conn.close()
 
-    cursor = conn.cursor()
-    query = '''SELECT name FROM sys.databases'''
-    cursor.execute(query)
-    data = cursor.fetchall()
-    cursor.close()
-    conn.close()
-    
-    databases = []
-    for i in data:
-        if i[0] != "master" and i[0] != "msdb" and i[0] != "model" and i[0] != "Resource" and i[0] != "tempdb":
-            databases.append(i[0])
+	databases = []
+	for i in data:
+		# Exclude system databases from results
+		if i[0] != "master" and i[0] != "msdb" and i[0] != "model" and i[0] != "Resource" and i[0] != "tempdb":
+			databases.append(i[0])
 
-    return databases;
-        
-def getTables(driver, server, user_name, password, database):
-    try:
-        if isSQLAuth(user_name, password):
-                conn = pyodbc.connect(driver=driver, host=server, uid=user_name, pwd=password, database=database, trusted_connection='no')
-        else:
-            conn = pyodbc.connect(driver=driver, host=server, database=database, trusted_connection='yes')
-    except:
-        return []
+	return databases;
+		
+def getTables(connString, database, timeout):
+	if (not testConnection(connString, timeout)):
+		return 1
 
-    cursor = conn.cursor()
-    query = f'''SELECT * FROM {database}.sys.tables'''
-    cursor.execute(query)
-    data = cursor.fetchall()
-    cursor.close()
-    conn.close()
+	conn = pyodbc.connect(connString, timeout=timeout)
+	cursor = conn.cursor()
+	query = f'''SELECT * FROM {database}.sys.tables'''
+	cursor.execute(query)
+	data = cursor.fetchall()
+	cursor.close()
+	conn.close()
 
-    tables = []
-    for i in data:
-        tables.append(i[0])
+	tables = []
+	for i in data:
+		tables.append(i[0])
 
-    return tables;
+	return tables;
 
+def getColumns(connString, table, timeout):
+	if (not testConnection(connString, timeout)):
+		return 1
+	
+	conn = pyodbc.connect(connString, timeout=timeout)
+	cursor = conn.cursor()
+	query = f'''SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = \'{table}\''''
+	cursor.execute(query)
+	data = cursor.fetchall()
+	cursor.close()
+	conn.close()
+
+	columns = []
+	for i in data:
+		columns.append(i[0])
+	
+	return columns
 
 # # connection string using the details above
 # cnxn = pyodbc.connect(driver=driver, host=server, database=db, trusted_connection='yes')
@@ -110,7 +104,7 @@ def getTables(driver, server, user_name, password, database):
 
 #        if d <= radius:
 #            results.insert(len(results), (centralPoint, i, d))
-            #print((centralPoint + 1, i + 1, d))
+			#print((centralPoint + 1, i + 1, d))
 
 #t1 = time.time()
 #print("len(results) =", len(results))
@@ -135,7 +129,7 @@ def getTables(driver, server, user_name, password, database):
    
 #            if d <= radius:
 #                results.insert(len(results), (centralPoint, i, d))
-                #print((centralPoint + 1, i + 1, d))
+				#print((centralPoint + 1, i + 1, d))
 
 #t1 = time.time()
 
@@ -148,128 +142,125 @@ def getTables(driver, server, user_name, password, database):
 ### Improvements: Now takes into account longitude, ruling out what has too high of a longitude value based on the radius given. Now has a "square radius" with somewhat of an error margin to narrow results down. ###
 
 def inRadius(pointID, radius, fileDestination):
-    def callBack():
-        t0 = time.time()
+	t0 = time.time()
 
-        centralPoint = functions.linearSearch(pointID, data)
+	centralPoint = functions.linearSearch(pointID, data)
 
-        latPerMile = 0.01459854014 # lat/mile, or 1/68.5 (68.5 for error margin)
+	latPerMile = 0.01459854014 # lat/mile, or 1/68.5 (68.5 for error margin)
 
-        upperBound = data[centralPoint][1] + (radius * latPerMile)
-        lowerBound = data[centralPoint][1] - (radius * latPerMile)
-        leftBound = data[centralPoint][2] - abs(1/(math.cos(data[centralPoint][1]) * 69.172) * radius)
-        rightBound = data[centralPoint][2] + abs(1/(math.cos(data[centralPoint][1]) * 69.172) * radius)
+	upperBound = data[centralPoint][1] + (radius * latPerMile)
+	lowerBound = data[centralPoint][1] - (radius * latPerMile)
+	leftBound = data[centralPoint][2] - abs(1/(math.cos(data[centralPoint][1]) * 69.172) * radius)
+	rightBound = data[centralPoint][2] + abs(1/(math.cos(data[centralPoint][1]) * 69.172) * radius)
 
-        results = []
-        for i in range (0, len(data)):
-            if i != centralPoint:
-                if data[i][0] >= lowerBound and data[i][1] <= upperBound: # Checks if i'th point is within latitude range of radius
-                    if data[i][1] >= leftBound and data[i][2] <= rightBound: # Checks if i'th point is within longitude range of radius
-                        d = functions.vincenty((data[centralPoint][1], data[centralPoint][2]), (data[i][1], data[i][2]), miles=True)
+	results = []
+	for i in range (0, len(data)):
+		if i != centralPoint:
+			if data[i][0] >= lowerBound and data[i][1] <= upperBound: # Checks if i'th point is within latitude range of radius
+				if data[i][1] >= leftBound and data[i][2] <= rightBound: # Checks if i'th point is within longitude range of radius
+					d = functions.vincenty((data[centralPoint][1], data[centralPoint][2]), (data[i][1], data[i][2]), miles=True)
 
-                        if d <= radius:
-                            results.insert(len(results), [data[centralPoint][0], data[centralPoint][1], data[centralPoint][2], data[i][0], data[i][1], data[i][2], functions.vincenty((data[centralPoint][1], data[centralPoint][2]), (data[i][1], data[i][2]), miles=True)])
+					if d <= radius:
+						results.insert(len(results), [data[centralPoint][0], data[centralPoint][1], data[centralPoint][2], data[i][0], data[i][1], data[i][2], functions.vincenty((data[centralPoint][1], data[centralPoint][2]), (data[i][1], data[i][2]), miles=True)])
 
-        t1 = time.time()
+	t1 = time.time()
 
-        functions.quickSort(results, 0, len(results) - 1, 6)
+	functions.quickSort(results, 0, len(results) - 1, 6)
 
-        print("\nTime:", t1 - t0)
-        print(fileDestination + "/output.csv")
-        f = open((fileDestination + "/output.csv"), "x")
+	print("\nTime:", t1 - t0)
+	print(fileDestination + "/output.csv")
+	f = open((fileDestination + "/output.csv"), "x")
 
-        f.write("ID1,ID1_Lat,ID1_Long,ID2,ID2_Lat,ID2_Long,Distance\n")
+	f.write("ID1,ID1_Lat,ID1_Long,ID2,ID2_Lat,ID2_Long,Distance\n")
 
-        for i in range(len(results)):
-            for j in range(len(results[i])):
-                f.write(str(results[i][j]))
-                if j != len(results[i]) - 1:
-                    f.write(",")
+	for i in range(len(results)):
+		for j in range(len(results[i])):
+			f.write(str(results[i][j]))
+			if j != len(results[i]) - 1:
+				f.write(",")
 
-            if i != len(results) - 1:
-                f.write("\n")
+		if i != len(results) - 1:
+			f.write("\n")
 
-        f.close()
-    t = threading.Thread(target=callBack)
-    t.start()
+	f.close()
 
 # ########## Closest Neighbors METHOD 1 ##########
 
 results = []
 
 def withinBounds(pointID, radius):
-    global data
-    global results
+	global data
+	global results
 
-    results = []
+	results = []
 
-    centralPoint = functions.linearSearch(pointID, data)
-    latPerMile = 0.01459854014 # lat/mile, or 1/68.5 (68.5 for error margin)
+	centralPoint = functions.linearSearch(pointID, data)
+	latPerMile = 0.01459854014 # lat/mile, or 1/68.5 (68.5 for error margin)
 
-    upperBound = data[centralPoint][1] + (radius * latPerMile)
-    lowerBound = data[centralPoint][1] - (radius * latPerMile)
-    leftBound = data[centralPoint][2] - abs(1/(math.cos(data[centralPoint][1]) * 69.172) * radius)
-    rightBound = data[centralPoint][2] + abs(1/(math.cos(data[centralPoint][1]) * 69.172) * radius)
+	upperBound = data[centralPoint][1] + (radius * latPerMile)
+	lowerBound = data[centralPoint][1] - (radius * latPerMile)
+	leftBound = data[centralPoint][2] - abs(1/(math.cos(data[centralPoint][1]) * 69.172) * radius)
+	rightBound = data[centralPoint][2] + abs(1/(math.cos(data[centralPoint][1]) * 69.172) * radius)
 
-    for i in range (0, len(data)):
-        if i != centralPoint:
-            if data[i][1] >= lowerBound and data[i][1] <= upperBound: # Checks if i'th point is within latitude range of radius
-                if data[i][2] >= leftBound and data[i][2] <= rightBound: # Checks if i'th point is within longitude range of radius
-                    results.insert(len(results), [data[centralPoint][0], data[centralPoint][1], data[centralPoint][2], data[i][0], data[i][1], data[i][2], functions.vincenty((data[centralPoint][1], data[centralPoint][2]), (data[i][1], data[i][2]), miles=True)])
+	for i in range (0, len(data)):
+		if i != centralPoint:
+			if data[i][1] >= lowerBound and data[i][1] <= upperBound: # Checks if i'th point is within latitude range of radius
+				if data[i][2] >= leftBound and data[i][2] <= rightBound: # Checks if i'th point is within longitude range of radius
+					results.insert(len(results), [data[centralPoint][0], data[centralPoint][1], data[centralPoint][2], data[i][0], data[i][1], data[i][2], functions.vincenty((data[centralPoint][1], data[centralPoint][2]), (data[i][1], data[i][2]), miles=True)])
 
-    return 0
+	return 0
 
 def closestNeighbor(pointID, neighbors):
-    global data
-    global results
+	global data
+	global results
 
-    t0 = time.time()
+	t0 = time.time()
 
-    results = []
+	results = []
 
-    radius = 5
+	radius = 5
 
-    lastAction = None
+	lastAction = None
 
-    while (len(results) < neighbors) and len(results) != neighbors:
-        results = []
-        withinBounds(pointID, radius)
+	while (len(results) < neighbors) and len(results) != neighbors:
+		results = []
+		withinBounds(pointID, radius)
 
-        if len(results) == neighbors:
-            break
-        elif len(results) > neighbors:
-            if lastAction and lastAction < 1:
-                radius *= lastAction - .25
-                lastAction -= .25
-            else:
-                radius *= .75
-                lastAction = .75
-        elif len(results) < neighbors:
-            if lastAction and lastAction > 1:
-                radius *= lastAction + .25
-                lastAction += .25
-            else:
-                radius *= 1.25
-                lastAction = 1.25
+		if len(results) == neighbors:
+			break
+		elif len(results) > neighbors:
+			if lastAction and lastAction < 1:
+				radius *= lastAction - .25
+				lastAction -= .25
+			else:
+				radius *= .75
+				lastAction = .75
+		elif len(results) < neighbors:
+			if lastAction and lastAction > 1:
+				radius *= lastAction + .25
+				lastAction += .25
+			else:
+				radius *= 1.25
+				lastAction = 1.25
 
-    functions.quickSort(results, 0, len(results) - 1, 6)
-    del results[len(results) - (len(results) - neighbors) : len(results)]
+	functions.quickSort(results, 0, len(results) - 1, 6)
+	del results[len(results) - (len(results) - neighbors) : len(results)]
 
-    t1 = time.time()
+	t1 = time.time()
 
-    print("Time:", t1 - t0)
+	print("Time:", t1 - t0)
 
-    f = open("output.csv", "x")
+	f = open("output.csv", "x")
 
-    f.write("ID1,ID1_Lat,ID1_Long,ID2,ID2_Lat,ID2_Long,Distance\n")
+	f.write("ID1,ID1_Lat,ID1_Long,ID2,ID2_Lat,ID2_Long,Distance\n")
 
-    for i in range(len(results)):
-        for j in range(len(results[i])):
-            f.write(str(results[i][j]))
-            if j != len(results[i]) - 1:
-                f.write(",")
+	for i in range(len(results)):
+		for j in range(len(results[i])):
+			f.write(str(results[i][j]))
+			if j != len(results[i]) - 1:
+				f.write(",")
 
-        if i != len(results) - 1:
-            f.write("\n")
+		if i != len(results) - 1:
+			f.write("\n")
 
-    f.close()
+	f.close()
